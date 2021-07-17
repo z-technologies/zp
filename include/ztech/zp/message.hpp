64288866af4069aae3c6be9926670625813da99a
Ayman Al-Qadhi <alqd@protonmail.com>
2021-07-17 19:59:38 +0300

    Add basic protocol header

---
+++ include/ztech/zp/message.hpp
@@ -0,0 +1,38 @@
+#ifndef ZTECH_ZP_MESSAGE_HPP
+#define ZTECH_ZP_MESSAGE_HPP
+
+#include "ztech/zp/flags.hpp"
+
+#include <cstdint>
+#include <type_traits>
+
+namespace ztech::zp {
+
+namespace detail {
+
+template <typename TExtra>
+concept is_command_error_code = requires {
+    std::is_enum_v<TExtra> &&std::is_base_of_v<std::uint16_t, TExtra>;
+};
+
+} // namespace detail
+
+inline namespace v1 {
+
+constexpr std::uint8_t version = 0x01;
+
+template <typename TExtra>
+requires ztech::zp::detail::is_command_error_code<TExtra>
+struct message_header {
+    std::uint8_t         version : 4;
+    std::uint16_t        type    : 12;
+    TExtra               command;
+    ztech::zp::v1::flags flags;
+    std::uint16_t        tag;
+    std::uint16_t        body_length;
+};
+
+} // namespace v1
+} // namespace ztech::zp
+
+#endif
