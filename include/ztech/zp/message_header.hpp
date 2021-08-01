e0aeac36cf4c18468523fb72c2393850cd8f0fa1
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-01 19:02:48 +0300

    Rewrtie message header

--- include/ztech/zp/message_header.hpp
+++ include/ztech/zp/message_header.hpp
@@ -4,33 +4,20 @@
 #include "ztech/zp/flags.hpp"
 
 #include <cstdint>
-#include <type_traits>
 
 namespace ztech::zp {
-
-namespace detail {
-
-template <typename TExtra>
-concept is_command_error_code = requires {
-    std::is_enum_v<TExtra> &&std::is_base_of_v<std::uint16_t, TExtra>;
-};
-
-} // namespace detail
-
 inline namespace v1 {
 
-constexpr std::uint8_t version = 0x01;
+constexpr std::uint8_t protocol_version{0x00};
 
 template <typename TExtra>
-requires ztech::zp::detail::is_command_error_code<TExtra>
 struct message_header {
     std::uint8_t         version : 4;
     std::uint16_t        type    : 12;
-    TExtra               command;
-    ztech::zp::v1::flags flags;
+    std::uint16_t        command;
     std::uint16_t        tag;
+    ztech::zp::v1::flags flags;
     std::uint16_t        body_length;
-    std::uint16_t        actual_length;
 };
 
 } // namespace v1
