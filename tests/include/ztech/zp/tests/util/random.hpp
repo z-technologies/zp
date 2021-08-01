a6a0af674339c36dd5320f80f8d812611c3d0c22
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-01 19:01:13 +0300

    Add utility random values generating functions

---
+++ tests/include/ztech/zp/tests/util/random.hpp
@@ -0,0 +1,95 @@
+#ifndef ZTECH_ZP_TESTS_UTIL_RANDOM_HPP
+#define ZTECH_ZP_TESTS_UTIL_RANDOM_HPP
+
+#include "fmt/format.h"
+
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <limits>
+#include <random>
+#include <string>
+#include <type_traits>
+
+namespace ztech::zp::tests::util {
+
+enum class random_string_options : std::uint32_t {
+    alpha      = 1,
+    num        = 2,
+    sym        = 4,
+    alphanum   = alpha | num,
+    lower_case = 8,
+    upper_case = 16,
+    mixed_case = lower_case | upper_case,
+    mixed      = alpha | num | lower_case | upper_case | sym,
+};
+
+inline auto operator&(random_string_options lhs, random_string_options rhs) {
+    return static_cast<std::uint32_t>(lhs) & static_cast<std::uint32_t>(rhs);
+}
+
+inline auto operator|(random_string_options lhs, random_string_options rhs) {
+    return static_cast<random_string_options>(static_cast<std::uint32_t>(lhs) |
+                                              static_cast<std::uint32_t>(rhs));
+}
+
+auto random_string(std::size_t len, random_string_options opts) -> std::string {
+    std::mt19937 rng{std::random_device{}()};
+
+    constexpr auto lower_case_alpha = "abcdefghijklmnopqrstuvwxyz";
+    constexpr auto upper_case_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    constexpr auto num              = "1234567890";
+    constexpr auto sym              = "`~!@#$%^&*()-_=+\"\\/?.>,<";
+
+    std::string pool;
+    std::string ret;
+
+    if ((opts & random_string_options::alpha) != 0U) {
+        if (((opts & random_string_options::lower_case) == 0U) &&
+            ((opts & random_string_options::upper_case) == 0U)) {
+            fmt::format_to(std::back_inserter(pool), lower_case_alpha);
+        } else {
+            if ((opts & random_string_options::lower_case) != 0U) {
+                fmt::format_to(std::back_inserter(pool), lower_case_alpha);
+            }
+
+            if ((opts & random_string_options::upper_case) != 0U) {
+                fmt::format_to(std::back_inserter(pool), upper_case_alpha);
+            }
+        }
+    }
+
+    if ((opts & random_string_options::num) != 0U) {
+        fmt::format_to(std::back_inserter(pool), num);
+    }
+
+    if ((opts & random_string_options::sym) != 0U) {
+        fmt::format_to(std::back_inserter(pool), sym);
+    }
+
+    std::shuffle(pool.begin(), pool.end(), rng);
+    std::uniform_int_distribution<std::size_t> dist{0, pool.size() - 1};
+
+    while ((len--) != 0U) {
+        fmt::format_to(std::back_inserter(ret), "{}", pool[dist(rng)]);
+    }
+
+    return ret;
+}
+
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T> ||
+                                                  std::is_floating_point_v<T>>>
+auto random(T min = std::numeric_limits<T>::min(),
+         T max = std::numeric_limits<T>::max()) -> T {
+    std::mt19937 rng{std::random_device{}()};
+
+    if constexpr (std::is_integral_v<T>) {
+        return std::uniform_int_distribution<T>{min, max}(rng);
+    } else {
+        return std::uniform_real_distribution<T>{min, max}(rng);
+    }
+}
+
+} // namespace ztech::zp::tests::util
+
+#endif
