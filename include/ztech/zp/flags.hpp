e3529b48216f966cf62d0be230f0feb99411daf3
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-01 19:02:18 +0300

    Rewrite flags, and add a negation operator

--- include/ztech/zp/flags.hpp
+++ include/ztech/zp/flags.hpp
@@ -2,18 +2,17 @@
 #define ZTECH_ZP_FLAGS_HPP
 
 #include <cstdint>
-#include <type_traits>
 
 namespace ztech::zp {
 inline namespace v1 {
 
 enum class flags : std::uint16_t {
-    none                 = 0,
-    end_of_conversation  = 1 << 0,
-    terminate_connection = 1 << 1,
-    checksum             = 1 << 2,
-    ack                  = 1 << 3,
-    compress             = 1 << 4,
+    none           = 0,
+    end_of_message = 1U << 0U,
+    end_of_stream  = 1U << 1U,
+    end_of_session = 1U << 2U,
+    has_checksum   = 1U << 3U,
+    requires_ack   = 1U << 4U,
 };
 
 }
@@ -46,6 +45,10 @@ inline auto operator&=(flags &rhs, const flags &lhs) -> flags & {
 inline auto operator^=(flags &rhs, const flags &lhs) -> flags & {
     rhs = rhs ^ lhs;
     return rhs;
+}
+
+inline auto operator~(const flags &lhs) -> flags {
+    return static_cast<flags>(~static_cast<std::uint16_t>(lhs));
 }
 
 } // namespace ztech::zp
