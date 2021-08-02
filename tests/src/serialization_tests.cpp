eac6b8e8f26a6caccf70cd9e71b95b4284c88a9d
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-02 14:58:09 +0300

    Cover all test cases

--- tests/src/serialization_tests.cpp
+++ tests/src/serialization_tests.cpp
@@ -1,35 +1,67 @@
 #include "ztech/zp/tests/util/random.hpp"
 #include "ztech/zp/util/serialization.hpp"
 
-#include "msgpack/sbuffer.hpp"
 #include "gtest/gtest.h"
 
-#include "msgpack/adaptor/bool.hpp"
-#include "msgpack/adaptor/float.hpp"
-#include "msgpack/adaptor/int.hpp"
+#include "msgpack/adaptor/string.hpp"
 
 #include <cstdint>
 #include <iostream>
+#include <string>
 #include <type_traits>
 #include <vector>
 
+using namespace ztech::zp::tests::util;
+
 namespace ztech::zp::tests {
+namespace {
+
+constexpr auto test_string_size = 0xFF;
+
+struct test_model {
+    std::uint8_t  uint8_value;
+    std::uint16_t uint16_value;
+    std::uint32_t uint32_value;
+    std::uint64_t uint64_value;
+    std::int8_t   int8_value;
+    std::int16_t  int16_value;
+    std::int32_t  int32_value;
+    std::int64_t  int64_value;
+
+    bool   bool_value;
+    float  float_value;
+    double double_value;
+
+    std::string string_value;
+
+    inline auto operator==(const test_model& rhs) const noexcept
+        -> bool = default;
+
+    ZP_DEFINE(uint8_value, uint16_value, uint32_value, uint64_value, int8_value,
+              int16_value, int32_value, int64_value, bool_value, float_value,
+              double_value, string_value);
+};
+
+template <typename T, typename Container>
+inline void test_type(Container& c, const T& value) {
+    c.clear();
+    ztech::zp::util::serialize(c, value);
+
+    T    out_value{};
+    auto success = ztech::zp::util::deserialize(c, out_value);
+
+    ASSERT_EQ(value, out_value);
+    ASSERT_TRUE(success);
+}
 
 template <typename T, typename Container>
 requires std::is_fundamental_v<T> &&
     ztech::zp::util::detail::writable_buffer<Container>
 inline void test_fundamental_type(Container& c) {
-    auto random_value = ztech::zp::tests::util::random<T>();
-
-    c.clear();
-    ztech::zp::util::serialize(c, random_value);
-
-    T    out_value{};
-    auto success = ztech::zp::util::deserialize(c, out_value);
-
-    ASSERT_EQ(random_value, out_value);
-    ASSERT_TRUE(success);
+    test_type(c, random<T>());
 }
+
+} // namespace
 
 TEST(SerializationTests, STLPrimitiveTypesTest) { // !NOLINT
     std::vector<std::uint8_t>              buf{};
@@ -54,6 +86,36 @@ TEST(SerializationTests, MsgPackBufferPrimitiveTypesTest) { // !NOLINT
     test_fundamental_type<std::uint64_t>(buf);
     test_fundamental_type<float>(buf);
     test_fundamental_type<double>(buf);
+}
+
+TEST(SerializationTests, StringTest) { // !NOLINT
+    msgpack::sbuffer buf{};
+
+    auto random_str =
+        random_string(test_string_size, random_string_options::mixed);
+
+    test_type(buf, random_str);
+}
+
+TEST(SerializationTests, ModelTest) { // !NOLINT
+    msgpack::sbuffer buf{};
+
+    test_model random_model{
+        .uint8_value  = random<std::uint8_t>(),
+        .uint16_value = random<std::uint16_t>(),
+        .uint32_value = random<std::uint32_t>(),
+        .uint64_value = random<std::uint64_t>(),
+        .int8_value   = random<std::int8_t>(),
+        .int16_value  = random<std::int16_t>(),
+        .int32_value  = random<std::int32_t>(),
+        .int64_value  = random<std::int64_t>(),
+        .bool_value   = random<bool>(),
+        .float_value  = random<float>(),
+        .double_value = random<double>(),
+        .string_value =
+            random_string(test_string_size, random_string_options::mixed)};
+
+    test_type(buf, random_model);
 }
 
 } // namespace ztech::zp::tests
