5871b2dd031e742cecc8e895e6269a397e453c2f
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-11 01:26:56 +0300

    Modify header encoding methods to include version

--- src/message_header.cpp
+++ src/message_header.cpp
@@ -11,18 +11,21 @@ namespace ztech::zp {
 
 void message_header::encode(
     std::array<std::uint8_t, size>& buf) const noexcept {
-    constexpr std::size_t type_offset{0UL};
+    constexpr std::size_t version_offset{0UL};
+    constexpr std::size_t type_offset{version_offset + sizeof(version)};
     constexpr std::size_t command_offset{type_offset + sizeof(type)};
     constexpr std::size_t flags_offset{command_offset + sizeof(command)};
     constexpr std::size_t tag_offset{flags_offset + sizeof(flags)};
     constexpr std::size_t body_length_offset{tag_offset + sizeof(tag)};
 
-    static_assert(type_offset == 0UL);
-    static_assert(command_offset == 2UL);
-    static_assert(flags_offset == 4UL);
+    static_assert(version_offset == 0UL);
+    static_assert(type_offset == 1UL);
+    static_assert(command_offset == 3UL);
+    static_assert(flags_offset == 5UL);
     static_assert(tag_offset == 6UL);
     static_assert(body_length_offset == 10UL);
 
+    ztech::zp::detail::write_uint<version_offset>(version, buf);
     ztech::zp::detail::write_uint<type_offset>(type, buf);
     ztech::zp::detail::write_uint<command_offset>(command, buf);
     ztech::zp::detail::write_uint<flags_offset>(
@@ -32,6 +35,7 @@ void message_header::encode(
 }
 
 void message_header::encode(std::vector<std::uint8_t>& buf) const noexcept {
+    ztech::zp::detail::append_uint(version, buf);
     ztech::zp::detail::append_uint(type, buf);
     ztech::zp::detail::append_uint(command, buf);
     ztech::zp::detail::append_uint(static_cast<flags_underlying_type>(flags),
