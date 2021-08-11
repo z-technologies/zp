b1d29fe6236d5308732578bb657ee2d9815dc589
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-11 20:03:53 +0300

    Implement header encoding into a vector

--- src/message_header.cpp
+++ src/message_header.cpp
@@ -1,54 +1,43 @@
 #include "ztech/zp/message_header.hpp"
+#include "ztech/zp/detail/encoding.hpp"
 
 #include <boost/endian.hpp>
 
 #include <array>
 #include <cstdint>
-#include <cstring>
-#include <type_traits>
 #include <vector>
-
-namespace {
-
-template <std::size_t offset, typename T, std::size_t size = sizeof(T)>
-requires std::is_unsigned_v<T>
-inline void
-encode_uint(T                                                          val,
-            std::array<std::uint8_t, ztech::zp::message_header::size>& buf) {
-    static_assert(size >= 1 && size <= 4 && size <= sizeof(T));
-    static_assert(offset + size <= ztech::zp::message_header::size);
-
-    buf[offset + sizeof(T) - size] =
-        (val >> (8 * (size - 1))) & 0xFF; // !NOLINT
-
-    if constexpr (size - 1 > 0) {
-        encode_uint<offset, T, size - 1>(val, buf);
-    }
-}
-
-} // namespace
 
 namespace ztech::zp {
 
 void message_header::encode(
     std::array<std::uint8_t, size>& buf) const noexcept {
-    constexpr std::size_t type_offset{0};
+    constexpr std::size_t type_offset{0UL};
     constexpr std::size_t command_offset{type_offset + sizeof(type)};
     constexpr std::size_t flags_offset{command_offset + sizeof(command)};
     constexpr std::size_t tag_offset{flags_offset + sizeof(flags)};
     constexpr std::size_t body_length_offset{tag_offset + sizeof(tag)};
 
-    static_assert(type_offset == 0);
-    static_assert(command_offset == 2);
-    static_assert(flags_offset == 4);
-    static_assert(tag_offset == 6);
-    static_assert(body_length_offset == 10);
+    static_assert(type_offset == 0UL);
+    static_assert(command_offset == 2UL);
+    static_assert(flags_offset == 4UL);
+    static_assert(tag_offset == 6UL);
+    static_assert(body_length_offset == 10UL);
 
-    ::encode_uint<type_offset>(type, buf);
-    ::encode_uint<command_offset>(command, buf);
-    ::encode_uint<flags_offset>(static_cast<flags_underlying_type>(flags), buf);
-    ::encode_uint<tag_offset>(tag, buf);
-    ::encode_uint<body_length_offset>(body_length, buf);
+    ztech::zp::detail::write_uint<type_offset>(type, buf);
+    ztech::zp::detail::write_uint<command_offset>(command, buf);
+    ztech::zp::detail::write_uint<flags_offset>(
+        static_cast<flags_underlying_type>(flags), buf);
+    ztech::zp::detail::write_uint<tag_offset>(tag, buf);
+    ztech::zp::detail::write_uint<body_length_offset>(body_length, buf);
+}
+
+void message_header::encode(std::vector<std::uint8_t>& buf) const noexcept {
+    ztech::zp::detail::append_uint(type, buf);
+    ztech::zp::detail::append_uint(command, buf);
+    ztech::zp::detail::append_uint(static_cast<flags_underlying_type>(flags),
+                                   buf);
+    ztech::zp::detail::append_uint(tag, buf);
+    ztech::zp::detail::append_uint(body_length, buf);
 }
 
 } // namespace ztech::zp
