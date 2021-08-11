8c379040218e61ab0dd91c7c0ba35b1626a6fde1
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-11 18:33:08 +0300

    Add utility encoding functions

---
+++ include/ztech/zp/detail/encoding.hpp
@@ -0,0 +1,49 @@
+#ifndef ZTECH_ZP_DETAIL_ENCODING_HPP
+#define ZTECH_ZP_DETAIL_ENCODING_HPP
+
+#include "ztech/zp/message_header.hpp"
+
+#include <array>
+#include <cstddef>
+#include <type_traits>
+#include <vector>
+
+namespace ztech::zp::detail {
+
+template <std::size_t i, typename T>
+requires std::is_unsigned_v<T>
+constexpr auto get_nth_byte(T value) -> std::uint8_t {
+    static_assert(i < sizeof(T));
+
+    return (value >> (8 * (sizeof(T) - i - 1))) & 0xFF;
+}
+
+template <std::size_t offset, typename T, std::size_t array_size,
+          std::size_t i = 0>
+requires std::is_unsigned_v<T>
+void write_uint(T value, std::array<std::uint8_t, array_size>& buf) {
+    static_assert(i < sizeof(T));
+    static_assert(offset + i + 1 <= array_size);
+
+    buf[offset + i] = get_nth_byte<i>(value);
+
+    if constexpr (i < sizeof(T) - 1) {
+        write_uint<offset, T, array_size, i + 1>(value, buf);
+    }
+}
+
+template <typename T, std::size_t i = 0>
+requires std::is_unsigned_v<T>
+void append_uint(T value, std::vector<std::uint8_t>& buf) {
+    static_assert(i < sizeof(T));
+
+    buf.emplace_back(get_nth_byte<i>(value));
+
+    if constexpr (i < sizeof(T) - 1) {
+        append_uint<T, i + 1>(value, buf);
+    }
+}
+
+} // namespace ztech::zp::detail
+
+#endif
