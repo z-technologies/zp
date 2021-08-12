5f30979193d049063409d124380b56461cdf6cb0
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-12 06:17:48 +0300

    Rewrite message tests

--- tests/src/message_tests.cpp
+++ tests/src/message_tests.cpp
@@ -10,32 +10,46 @@
 #include <type_traits>
 #include <vector>
 
-using ztech::zp::tests::util::random;
-using ztech::zp::util::message_builder;
+using namespace ztech::zp::tests::util;
 
-namespace ztech::zp::tests {
-
-TEST(MessageTests, FlattenTest) { // !NOLINT
+namespace {
+template <std::uint8_t version, bool is_request>
+void test_message() {
     const auto test_type    = random<std::uint16_t>();
     const auto test_command = random<std::uint16_t>();
     const auto test_tag     = random<std::uint16_t>();
 
-    const auto message = message_builder{}
-                             .with_type(test_type)
-                             .with_command(test_command)
-                             .with_tag(test_tag)
-                             .build();
+    auto test_body = random_vector<std::uint8_t, 0xFF>();
+    ztech::zp::message_header<version, is_request> header{
+        .type        = test_type,
+        .command     = test_command,
+        .tag         = test_tag,
+        .body_length = static_cast<std::uint32_t>(test_body.size())};
+
+    ztech::zp::message<version, is_request> msg{std::move(header),
+                                                std::move(test_body)};
 
     std::vector<std::uint8_t> test_buf{};
-    message.header().encode(test_buf);
-    std::copy(std::cbegin(message.body()), std::cend(message.body()),
+    msg.header().encode(test_buf);
+    std::copy(std::cbegin(msg.body()), std::cend(msg.body()),
               std::back_inserter<>(test_buf));
 
-    const auto ret_buf = message.flatten();
+    const auto ret_buf = msg.flatten();
 
     ASSERT_EQ(test_buf.size(), ret_buf.size());
     ASSERT_TRUE(std::equal(std::cbegin(test_buf), std::cend(test_buf),
                            std::cbegin(ret_buf), std::cend(ret_buf)));
+}
+} // namespace
+
+namespace ztech::zp::tests {
+
+TEST(MessageTests, RequestFlattenTest) { // !NOLINT
+    ::test_message<0x07, true>();
+}
+
+TEST(MessageTests, ResponseFlattenTest) { // !NOLINT
+    ::test_message<0x07, false>();
 }
 
 } // namespace ztech::zp::tests
