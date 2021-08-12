92fad31f3aca7e0226814e0b1099675142a68999
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-12 05:27:10 +0300

    Fix message header tests

--- tests/src/message_header_tests.cpp
+++ tests/src/message_header_tests.cpp
@@ -14,16 +14,16 @@
 
 namespace {
 
-constexpr std::size_t header_size{ztech::zp::message_header::size};
+constexpr std::size_t header_size{ztech::zp::message_header_size};
 constexpr std::array<std::uint8_t, ::header_size> test_header_bytes{
     0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
     0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD};
 
-const ztech::zp::message_header test_header{.version     = 0x11,
-                                            .type        = 0x2233,
-                                            .command     = 0x4455,
-                                            .tag         = 0x66778899,
-                                            .body_length = 0xAABBCCDD};
+const ztech::zp::message_header<0x08, true> test_header{.type    = 0x2233,
+                                                        .command = 0x4455,
+                                                        .tag     = 0x66778899,
+                                                        .body_length =
+                                                            0xAABBCCDD};
 
 template <typename T, typename U>
 auto compare_containers(const T& c1, const U& c2) -> bool {
