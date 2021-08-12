781866b5acf21096cd58d994856b2d83a10965a2
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-12 06:18:07 +0300

    Rewrite message builder tests

--- tests/src/message_builder_tests.cpp
+++ tests/src/message_builder_tests.cpp
@@ -4,29 +4,43 @@
 
 #include "gtest/gtest.h"
 
+#include <algorithm>
 #include <cstdint>
 
-using ztech::zp::tests::util::random;
-using ztech::zp::util::message_builder;
+using namespace ztech::zp::tests::util;
 
-namespace ztech::zp::tests {
+namespace {
 
-TEST(MessageBuilderTests, HeaderBuildingTest) { // !NOLINT
+template <std::uint8_t version, bool is_request>
+void test_message_builder() {
     const auto test_type    = random<std::uint16_t>();
     const auto test_command = random<std::uint16_t>();
     const auto test_tag     = random<std::uint16_t>();
+    const auto test_body    = random_vector<std::uint8_t, 0xFF>();
 
-    auto message = message_builder{}
-                       .with_type(test_type)
-                       .with_command(test_command)
-                       .with_tag(test_tag)
-                       .build();
+    const auto msg = ztech::zp::util::make_message_builder<version, is_request>(
+                         test_type, test_command, test_tag)
+                         .with_body(test_body)
+                         .build();
 
-    EXPECT_EQ(message.header().type, test_type);
-    EXPECT_EQ(message.header().command, test_command);
-    EXPECT_EQ(message.header().tag, test_tag);
-    EXPECT_EQ(message.header().body_length, 0);
-    EXPECT_EQ(message.body().size(), 0);
+    EXPECT_EQ(msg.header().type, test_type);
+    EXPECT_EQ(msg.header().command, test_command);
+    EXPECT_EQ(msg.header().tag, test_tag);
+    EXPECT_EQ(msg.header().body_length, test_body.size());
+    EXPECT_TRUE(std::equal(std::cbegin(test_body), std::cend(test_body),
+                           std::cbegin(msg.body()), std::cend(msg.body())));
+}
+
+} // namespace
+
+namespace ztech::zp::tests {
+
+TEST(MessageBuilderTests, TestRequestMessageBuilder) { // !NOLINT
+    ::test_message_builder<0x07, true>();
+}
+
+TEST(MessageBuilderTests, TestResponseMessageBuilder) { // !NOLINT
+    ::test_message_builder<0x07, false>();
 }
 
 } // namespace ztech::zp::tests
