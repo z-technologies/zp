21b976f07b5f9c51be553a967808861e3cfd558b
Ayman Al-Qadhi <alqd@protonmail.com>
2021-08-14 17:12:58 +0300

    Document encoding template functions

--- include/ztech/zp/detail/encoding.hpp
+++ include/ztech/zp/detail/encoding.hpp
@@ -9,6 +9,21 @@
 
 namespace ztech::zp::detail {
 
+/*!
+ * \breif Gets the byte with the index of \p i in \p value.
+ *
+ * This function gets the byte with the index of \p i in \p value. The index
+ * ranges from 0 to size of \p T, and from the most significant byte.
+ * \p T is required to be an unsigned integeral type (i.e., std::uint8_t,
+ * std::uint16_t, ...).
+ *
+ * \tparam i  The index of the byte to get
+ * \tparam T  The type of the value to get the byte from
+ *
+ * \param[in] value  The value to get the byte from
+ *
+ * \return The byte with the index of \p i
+ */
 template <std::size_t i, typename T>
 requires std::is_unsigned_v<T>
 constexpr auto get_nth_byte(T value) -> std::uint8_t {
@@ -17,6 +32,21 @@ constexpr auto get_nth_byte(T value) -> std::uint8_t {
     return (value >> (8 * (sizeof(T) - i - 1))) & 0xFF;
 }
 
+/*!
+ * \breif Gets the byte with the index of \p i in \p value.
+ *
+ * This function gets the byte with the index of \p i in \p value. The index
+ * ranges from 0 to size of \p T, and from the most significant byte.
+ * \p T is required to be an unsigned integeral type (i.e., std::uint8_t,
+ * std::uint16_t, ...).
+ *
+ * \tparam T  The type of the value to get the byte from
+ *
+ * \param[in] value  The value to get the byte from
+ * \param[in] i      The index of the byte to get
+ *
+ * \return The byte with the index of \p i
+ */
 template <typename T>
 requires std::is_unsigned_v<T>
 constexpr auto get_nth_byte(T value, std::size_t i) -> std::uint8_t {
@@ -25,6 +55,21 @@ constexpr auto get_nth_byte(T value, std::size_t i) -> std::uint8_t {
     return (value >> (8 * (sizeof(T) - i - 1))) & 0xFF;
 }
 
+/*!
+ * \brief Inserts the bytes of \p value into the passed array in big endian
+ *        order in the passed offset specified by \p offset.
+ *
+ * \tparam offset     The index from which to start writing the bytes of
+ *                    \p value
+ * \tparam T          The type of the value to be inserted. This type has to be
+ *                    of an unsigned integeral type
+ * \tparam array_size The size of the buffer \p buf into which to write the
+ *                    value
+ * \tparam i          The current index into which to write the current byte
+ *
+ * \param[in]  value The value which to be written into \p buf
+ * \param[out] buf   The buffer into which to write the value \p value
+ */
 template <std::size_t offset, typename T, std::size_t array_size,
           std::size_t i = 0>
 requires std::is_unsigned_v<T>
@@ -39,6 +84,16 @@ void write_uint(T value, std::array<std::uint8_t, array_size>& buf) {
     }
 }
 
+/*!
+ * \brief Appends the bytes of \p value to \p buf.
+ *
+ * \tparam T The type of the value to be inserted. This type has to be
+ *           of an unsigned integeral type
+ * \tparam i The current index into which to write the current byte
+ *
+ * \param[in]  value The value which to be written into \p buf
+ * \param[out] buf   The buffer into which to write the value \p value
+ */
 template <typename T, std::size_t i = 0>
 requires std::is_unsigned_v<T>
 void append_uint(T value, std::vector<std::uint8_t>& buf) {
